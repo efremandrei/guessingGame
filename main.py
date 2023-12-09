a36a5662c48454eca6b7bd8036229d9cf9e95d0e
Andrei Efremushkin <andrei.efr@gmail.com>
2023-12-09 16:28:05 +0200

    i think it's finised

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 
 expressionsList = [['always', 'deliver', 'quality'],
                    ['ask', 'powerful', 'questions'],
@@ -11,27 +11,60 @@ expressionsList = [['always', 'deliver', 'quality'],
                    ['knowledge', 'is', 'power'],
                    ['keep', 'it', 'simple']
                    ]
-# choose on of the expressions
-chosenExpression = expressionsList[random.randint(0, len(expressionsList)-1)]
-print(chosenExpression)
-parallelList = []
-# Create another list just made out of '_'s
-for word in chosenExpression:
-    parallelList.append('_'*len(word))
-print(parallelList)
+playAgain = True
+while playAgain:
+    # choose on of the expressions
+    chosenExpression = expressionsList[random.randint(0, len(expressionsList)-1)]
+    print(chosenExpression)
+    parallelList = []
+    # Create another list just made out of '_'s
+    for word in chosenExpression:
+        parallelList.append('_'*len(word))
+    print(parallelList)
 
-oneStringUnderscores = ' '.join(parallelList)
-oneStringExpression = ' '.join(chosenExpression)
+    oneStringUnderscores = ' '.join(parallelList)
+    oneStringExpression = ' '.join(chosenExpression)
 
-while '_' in oneStringUnderscores:
-    guess = input(f'The expression is {oneStringUnderscores}, please guess a character!')
-    if guess in oneStringExpression and guess not in oneStringUnderscores:
-        print("Good guess!/n")
-        indicesOfGuess = [index for index, char in enumerate(oneStringExpression) if char == guess]
-        for i in indicesOfGuess:
-            oneStringUnderscores = oneStringUnderscores[:i] + guess + oneStringExpression[i+1:]
-    elif guess in oneStringUnderscores:
-        print("Hey cheater! you've done that already! try something new!")
-        continue
-    elif guess not in oneStringExpression:
-        print("Nice try, not good enough though, let's try again...")
+    score = 0
+    startTime = 0
+    while '_' in oneStringUnderscores:
+        startTime = time.time()
+        guess = input(f'The expression is "{oneStringUnderscores}", please guess a character! \n')
+        if guess == 'exit':
+            print("Thanks for playing, hope you had fun! BYE!")
+            break
+        if len(guess) > 1:
+            print("Cheater cheater pants on...fire? \n Anyway, please enter only ONE character at a time!")
+            continue
+        if not guess.isalpha():
+            print("Hmmm, Please guess LETTERS ONLY\n")
+            continue
+        if guess in oneStringExpression and guess not in oneStringUnderscores:
+            print("Good guess!\n")
+            score +=5
+            indicesOfGuess = [index for index, char in enumerate(oneStringExpression) if char == guess]
+            for i in indicesOfGuess:
+                oneStringUnderscores = oneStringUnderscores[:i] + guess + oneStringUnderscores[i+1:]
+        elif guess in oneStringUnderscores:
+            print("Hey cheater! you've done that already! try something new!")
+            continue
+        elif guess not in oneStringExpression:
+            print("Nice try, not good enough though, let's try again...")
+            score -=1
+    if guess != 'exit':
+        if time.time()-startTime < 30:
+            print("You guessed right very fast! Get a bonus of 100 points! \n")
+            score += 100
+        print(f'Congratulation! you\'ve guessed the whole expression!\n it\'s: "{oneStringExpression}"\n '
+              f'And your score is: {score}\n')
+
+    while playAgain != 'Y' or playAgain != 'N':
+        playAgain = input("Would you like to play another round?(Y/N) \n")
+        if playAgain == 'N':
+            playAgain = False
+            print("thanks for playing the game...Bye bye!\n")
+            break
+        elif playAgain == 'Y':
+            playAgain = True
+            print ("Ok, so we're playing another round!\n")
+            break
